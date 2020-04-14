d9cbd1e5afb2131633c2ee3f1163b40564836035
mcruz <mcruzpr86@gmail.com>
2020-04-14 14:56:18 -0400

    for loops created for hints and answers

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+//JavaScript 
+    
+    // Here i need to place a table with content showing wins, the current word to guess, a hint picture, guesses remaining, letters guessed.
+    // create variable with guesses; Character names: Leonardo, Raphael, Michelangelo, Donatello, April, Splinter, Shredder
+    // hint colum should contain variation of photo or phrase
+    // create guessing area within table
+    // answer area with photo of correct guess
+    // create guessing loops with the letters of the alphabet
+    
+    var hints = ['Leader of the group', 'Has anger issues', 'Loves pizza', 'Gadget-wiz', 'Reporter', 'Master', 'Enemy of the turtles'];
+    var answers = ['Leonardo', 'Raphael', 'Michelangelo', 'Donatello', 'April', 'Splinter', 'Shredder'];
+
+    //console.log
+    console.log(hints[0]);
+    console.log(hints[1]);
+    console.log(hints[2]);
+    console.log(hints[3]);
+    console.log(hints[4]);
+    console.log(hints[5]);
+    console.log(hints[6]);
+    console.log(hints[7]);
+
+  
+
+    for (var i = 0; 1 < answers.length; i++) {
+      console.log(answers[i]);
+    }
+
+    //Loops go here
+
+
+
+
+
+    
