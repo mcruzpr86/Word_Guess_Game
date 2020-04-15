082cc1e06db43134c5122f50d033581ebddc919f
mcruz <mcruzpr86@gmail.com>
2020-04-14 23:27:15 -0400

    javascripts started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,26 +7,42 @@
     // answer area with photo of correct guess
     // create guessing loops with the letters of the alphabet
     
+   
+
     var hints = ['Leader of the group', 'Has anger issues', 'Loves pizza', 'Gadget-wiz', 'Reporter', 'Master', 'Enemy of the turtles'];
     var answers = ['Leonardo', 'Raphael', 'Michelangelo', 'Donatello', 'April', 'Splinter', 'Shredder'];
+    var guess = []
+    var lettersGuessed = []
+    var guessesRemaining = []
+    var wins = []
+    var lifesLeft = []
+
 
     //console.log
-    console.log(hints[0]);
-    console.log(hints[1]);
-    console.log(hints[2]);
-    console.log(hints[3]);
-    console.log(hints[4]);
-    console.log(hints[5]);
-    console.log(hints[6]);
-    console.log(hints[7]);
 
-  
+    for (var i = 0; i < hints.length; i++) {
+      console.log(hints[i]);
+    }
 
-    for (var i = 0; 1 < answers.length; i++) {
+    for (var i = 0; i < answers.length; i++) {
       console.log(answers[i]);
     }
 
+    // Word Generator
+    document.onkeydown = function (event) {
+      console.log(event.key)
+    }
+
+    function characterAnswers () {
+      var randomWord = answers[Math.floor (Math.random() * answers.length)];
+      console.log (randomWord)
+
+    }
+
+    
+    characterAnswers ()
     //Loops go here
+    
 
 
 
